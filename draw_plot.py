4ec2649d7c887cb6940083554d28b0a628c88e66
zwc662 <zwc662@gmail.com>
2018-09-11 15:03:47 -0400

    final-9.11

--- draw_plot.py
+++ draw_plot.py
@@ -2,26 +2,38 @@
 from pylab import *
 import numpy as np
 
+f = open('log150M-200000')
 
-f = open('log150M-150000000')
+# a = []
+# for i in f.readlines():
+# 	if i.startswith('Mean'):
+# 		a.append(float(i.split()[1]))
+# f. close()
+
+# b = []
+# f = open('log150M-150000000_before_pioson')
+# for i in f.readlines():
+# 	if i.startswith('Mean'):
+# 		b.append(float(i.split()[1]))
+# f. close()
 
 a = []
 for i in f.readlines():
-	if i.startswith('Mean'):
-		a.append(float(i.split()[1]))
+	if i.startswith('total'):
+		a.append(float(i.split()[7]))
 f. close()
 
 b = []
-f = open('log150M-150000000_poison')
+f = open('log150M-200000_poison')
 for i in f.readlines():
-	if i.startswith('Mean'):
-		b.append(float(i.split()[1]))
+	if i.startswith('total'):
+		b.append(float(i.split()[7]))
 f. close()
 
-x = np.linspace(101600, 132211200, 132)
+x = np.linspace(48000, 94400, 30)
 
 plot(x, a)
 plot(x, b)
 
-savefig("150M-150M.jpg")
+savefig("150M-0.2M_distribution.jpg")
 show()
